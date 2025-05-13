b55c8d722e86bf6973d1eb3437cc08447927944f
nicofavreg <nicolo.gasparini.95@gmail.com>
2025-05-13 10:53:51 +0200

    aggiuntp bottone

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 def main():
     st.title("Ciao a tutti")
-    st.button('click bottone')
-    st.balloons()
+    if st.button('click bottone'):
+        st.balloons()
 
 if __name__ == "__main__":
     main()
