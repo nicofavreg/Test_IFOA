2a235705143145e10aeb514fcaa2a0f3cdd98af5
nicofavreg <nicolo.gasparini.95@gmail.com>
2025-05-13 11:18:24 +0200

    aggiunta bravo

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ def main():
     st.title("Ciao a tutti")
     st.header("questa è la mia creazione di un'applicazione")
     if st.button('prova a cliccare'):
-        st.text("hai superato la prova!!")
+        st.text("hai superato la prova, bravo!!")
         st.image('C:/Users/ifoa/Desktop/Test_IFOA/lempuyang.jpg')
         st.balloons()
 
