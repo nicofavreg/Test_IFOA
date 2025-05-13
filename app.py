ea9d34dcdfad64fc02308c498f3b6b0b0035c7ea
nicofavreg <nicolo.gasparini.95@gmail.com>
2025-05-13 11:06:50 +0200

    aggiunta foto

--- app.py
+++ app.py
@@ -2,11 +2,10 @@ import streamlit as st
 
 def main():
     st.title("Ciao a tutti")
-    st.header("questa è la creazione di applicazione")
-    st.camera_input('take a photo with me')
-    st.image('https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vacanzeabali.it%2Ftempli%2Ftempio-lempuyang%2F&psig=AOvVaw3laoIFFCPD9jJWv-2wPVJe&ust=1747213426250000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJDC-a6LoI0DFQAAAAAdAAAAABAE')
+    st.header("questa è la mia creazione di un'applicazione")
     if st.button('prova a cliccare'):
         st.text("hai superato la prova!!")
+        st.image('C:/Users/ifoa/Desktop/Test_IFOA/lempuyang.jpg')
         st.balloons()
 
 if __name__ == "__main__":
