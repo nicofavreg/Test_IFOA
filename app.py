a3b2c02c1c42238d492c577b3a4a21a7b2bedee2
nicofavreg <nicolo.gasparini.95@gmail.com>
2025-05-13 11:21:59 +0200

    aggiunta bravo!

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ def main():
     st.header("questa è la mia creazione di un'applicazione")
     if st.button('prova a cliccare'):
         st.text("hai superato la prova, bravo!!")
-        st.image('C:/Users/ifoa/Desktop/Test_IFOA/lempuyang.jpg')
         st.balloons()
 
 if __name__ == "__main__":
