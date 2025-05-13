4b269fb98b138a7093b26a24c744f38845635c87
nicofavreg <nicolo.gasparini.95@gmail.com>
2025-05-13 11:28:03 +0200

    aggiunta immagine

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ def main():
     st.title("Ciao a tutti")
     st.header("questa è la mia creazione di un'applicazione")
     if st.button('prova a cliccare'):
-        st.text("hai superato la prova, bravo!!")
+        st.title("hai superato la prova, bravo!!")
         st.balloons()
+        st.image('lempuyang.jpg')
+    st.slider('test slider', 1, 10, 5)
 
 if __name__ == "__main__":
     main()
